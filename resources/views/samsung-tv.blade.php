823bd2456cfdb34272d354f8e0ec12796bf1d3b1
Sai Myo Min Aung <saimyominaung2110@gmail.com>
2023-06-18 16:02:26 +0630

    Finish Mulitple TV

--- resources/views/samsung-tv.blade.php
+++ resources/views/samsung-tv.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:url" content="{{ $imageUrl }}" />
+    {{-- <meta property="og:url" content="{{ $imageUrl }}" /> --}}
+    <meta property="og:url" content="{{ route('samsung-tv', ['id' => $gameUsedUser->photo]) }}" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="{{ $textData['tv_first_title'] ?? 'Samsung TV' }}" />
     <meta property="og:description" content="{{ $textData['tv_second_title'] ?? 'Samsung TV' }}" />
